de267e624239482966e0465c047082b452c084e2
kandelarjun67 <kandelarjun67@gmail.com>
2023-05-19 07:21:27 +0545

    login section updated

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link';
 export default function Home() {
   return (
    <div>
+
     hi login
+    
+    <Link href="/register">Register</Link>
    </div>
   )
 }
